f709279296b633b666c4021435d03a9197102b0b
Jayashree-VR <127225057+Jayashree-VR@users.noreply.github.com>
2025-02-04 21:18:03 +0530

    Create data_types.c

---
+++ data_types.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    float b=445.5;
+    char c='d';
+    char d[10]="sdf";
+
+    printf("%d\n", a);
+    printf("%f\n", b);
+    printf("%c\n", c);
+    printf("%s\n", d);
+
+    return 0;
+}
