ec33d5caa0a5c3d7a6e86969d65ddaa51a7a3a3d
Jayashree-VR <127225057+Jayashree-VR@users.noreply.github.com>
2025-02-04 21:31:13 +0530

    3

--- basic.c
+++ basic.c
@@ -3,5 +3,24 @@ int main()
 {
     int num=10;
     printf("%d",num);
+    
+    char var='a';
+    printf("%c",var);
+    
+    float n_1=10;
+    printf("%f",n_1);
+    
+    char name[15]="Jayashree VR";
+    printf("%s",name);
+    
+    int n_2=-10;
+    printf("%d",n_2);
+    
+    unsigned int n_3=-10;
+    printf("%u",n_3);
+    
+    int n_4=10;
+    printf("%o",n_4);
+    
     return 0;
 }
