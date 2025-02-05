670694a7fa15cee428e175706773b859e8916aa4
Jayashree-VR <127225057+Jayashree-VR@users.noreply.github.com>
2025-02-05 15:23:51 +0530

    7

--- 05_02_25.c
+++ 05_02_25.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
  int main() {
-    int n1,n2,n3;
-    float avg_1;
-    float avg_2;
-    n1=12;
-    n2=23;
-    n3=45;
-    avg_1=(n1+n2+n3)/3;
-    avg_2=(float)(n1+n2+n3)/3;
-    printf("avergage is %f\n",avg_1);
-     printf("avergage is %f",avg_2);
+    typedef int vr;
+    vr a=10;
+    printf("%d",a);
 
     return 0;
 }
