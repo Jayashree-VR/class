60fecc64a2f86005f63d80eec680bef0af7e910f
Jayashree-VR <127225057+Jayashree-VR@users.noreply.github.com>
2025-02-05 14:40:56 +0530

    Update 05_02_25.c

--- 05_02_25.c
+++ 05_02_25.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    unsigned char a=-1;
-;
-    printf("%d",a);
+    int a_1=56;
+    printf("%d\n",sizeof(a_1);
+    long long int a=9361201007;
+    printf("%lld\n",sizeof(a));
 
     return 0;
 }
