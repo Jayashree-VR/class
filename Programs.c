61ecd9ce73e893ff0334254cc1f9b635d8511047
Jayashree-VR <127225057+Jayashree-VR@users.noreply.github.com>
2025-02-06 10:09:29 +0530

    2

--- Programs.c
+++ Programs.c
@@ -78,6 +78,22 @@ int main() {
 
 //Swap two numbers
 
+#include <stdio.h>
+
+int main() {
+    int num_1,num_2,temp;
+    printf("Enter a value 1:");
+    scanf("%d",&num_1);
+    printf("Enter a value 2:");
+    scanf("%d",&num_2);
+    printf("The values before swapping are %d and %d",num_1,num_2);
+    temp=num_1;
+    num_1=num_2;
+    num_2=temp;
+    printf("\nThe swapped values are %d and %d",num_1,num_2);
+    
+    return 0;
+}
 
 //Odd or Even number
 
@@ -117,6 +133,56 @@ int main() {
 
 //check wheather a character is alphabet or not
 
+
 //Largest among 3 numbers
+
+#include <stdio.h>
+
+int main() {
+    int num_1,num_2,num_3;
+    printf("Enter a value 1:");
+    scanf("%d",&num_1);
+    printf("Enter a value 2:");
+    scanf("%d",&num_2);
+    printf("Enter a value 3:");
+    scanf("%d",&num_3);
+    
+    if(num_1>num_2 && num_1>num_3){
+        printf("Number 1 is greatest");
+    }
+    else if(num_2>num_1 && num_2>num_3){
+        printf("Number 2 is greatest");
+    }
+    else{
+        printf("Nuber 3 is greatest");
+    }
+    
+    
+    return 0;
+}
+
+//leap year
+
+#include <stdio.h>
+
+int main() {
+    int year;
+    printf("Enter a year:");
+    scanf("%d",&year);
+    
+    
+    if(((year%4==0) && (year%400==0)) || (year%100!=0)){
+        printf("It is a leap year");
+    }
+    else{
+        printf("It is not a leap year");
+    }
+    
+    
+    return 0;
+}
+//vowels and consonents
+
+
 
 //check if a number is divisible by 5 and 11
