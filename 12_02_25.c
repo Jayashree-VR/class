ad1e0fb8ce227d1615684f63d5975c09b9d60b9e
Jayashree-VR <127225057+Jayashree-VR@users.noreply.github.com>
2025-02-12 14:21:51 +0530

    1

---
+++ 12_02_25.c
@@ -0,0 +1,15 @@
+//Programs using nested for loop
+
+//1
+#include<stdio.h>
+int main(){
+    int row=3,col=2;
+    for(int i=0;i<row;i++){
+        printf("\n");
+        for(int j=0;j<col;j++){
+            printf("*");
+        }
+    }
+}
+
+//2
