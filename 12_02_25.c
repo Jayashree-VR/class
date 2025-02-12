9f05cbace015a8236f9789977d50f1f485e35e8e
Jayashree-VR <127225057+Jayashree-VR@users.noreply.github.com>
2025-02-12 14:28:35 +0530

    2

--- 12_02_25.c
+++ 12_02_25.c
@@ -13,3 +13,39 @@ int main(){
 }
 
 //2
+#include<stdio.h>
+int main(){
+    int row=3,col=2;
+    for(int i=1;i<=row;i++){
+        printf("\n");
+        for(int j=1;j<=col;j++){
+            printf("%d",i);
+        }
+    }
+}
+
+//3.
+
+#include<stdio.h>
+int main(){
+    int row=4,col=4;
+    for(int i=1;i<=row;i++){
+        printf("\n");
+        for(int j=1;j<=col;j++){
+            printf("%d",j);
+        }
+    }
+}
+
+//4.
+
+#include<stdio.h>
+int main(){
+    int row=4,col=4;
+    for(int i=row;i>0;i--){
+        printf("\n");
+        for(int j=col;j>0;j--){
+            printf("%d",i);
+        }
+    }
+}
